4385c4f53aa497ef890c5e380b76287295431c08
William Clavell <wjclavell28@gmail.com>
2020-10-15 12:22:36 -0400

    removed profile component from App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        < UserProfile />
         <a
           className="App-link"
           href="https://reactjs.org"
