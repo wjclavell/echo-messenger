cf52467a05041dda0d7751ea0b8f2d6fbd460830
William Clavell <wjclavell28@gmail.com>
2020-10-15 11:49:30 -0400

    styled profile form, created update method, created services folder and files

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import updateUser from './../services'
 
 class UserProfile extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      // probably have these values equal to the current user's info instead of empty string
       username: '',
       password: '',
-      avatar: ''
+      avatar: '',
+      error: null,
     }
   }
 
@@ -15,15 +18,24 @@ class UserProfile extends React.Component {
     this.setState({ [e.target.name]: e.target.value })
 }
 
-handleSubmit = () => {
-  console.log("submition, completion")
-  console.log(this.state)
-}
+  handleSubmit = async (e) => {
+  //* "this is where we will call the update user method from our services"
+    e.preventDefault();
+    const resp = await updateUser(this.state);
+    if (!resp) {
+        this.setState({
+            error: 'Could not update correctly.'
+        })
+    } else {
+        await this.props.setUser({username: resp.data.username, password: resp.data.password, avatar: resp.data.avatar});
+        this.props.history.push(`/main`);
+    }
+  }
 
 render () {
   return (
     <form className='profile'>
-      <h6>User Profile</h6>
+      <h3>User Profile</h3>
       <div className='profileField'>
           <label htmlFor='username'>USERNAME</label>
           <input type='text' name='username' placeholder='Change username' value={this.state.username} onChange={(e) => this.handleChange(e)}/>
@@ -38,7 +50,7 @@ render () {
       </div>
       <div className="updateUserButton">
           {this.state.error}
-          <Button type="submit" style={{backgroundColor: '#616161', marginTop: '2em'}} onClick={this.handleSubmit}>Save Changes</Button>
+          <Button type="submit" style={{backgroundColor: '#616161', marginTop: '3em'}} onClick={this.handleSubmit}>Save Changes</Button>
       </div>
     </form>
   )
