00260813dd45ff82d7bafa2eabe3cba039c02e11
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-10-16 15:46:47 -0400

    messages rendering

--- src/components/AllMsg.jsx
+++ src/components/AllMsg.jsx
@@ -12,8 +12,12 @@ class AllMsg extends React.Component {
         avatar: '',
         error: null,
         isOpen: false,
-        messages: null
+        messages: []
       }
+    }
+
+    componentDidMount = () => {
+        this.getMsgs()
     }
 
     getMsgs = async () => {
@@ -23,18 +27,36 @@ class AllMsg extends React.Component {
         })
     }
 
+
     render () {
-        const messages = this.getMsgs()
-        console.log(messages)
+        // console.log(this.state.messages.results)  
+        const allMsgs = this.state.messages.results && this.state.messages.results.reverse().map((msg, index) => {
+        console.log('this is in the all msgs ', msg)
+            return (
+                <div className="message-cont" key={index}>
+                    {/* <img src={msg.receiver.avatar}/> */}
+                    <h3>
+                        {msg.receiver_name}
+                    </h3>
+                    <p className="message-p">
+                        {msg.message}
+                    </p>
+                </div>
+            )
+
+
+        })
+
         // get messages and filter from most recent to least recent (use timestamps)
         // store most recent in a variable to display in the excerpt view of all messages
 
         return (
-            <div className="message">
+            <div className="all-messages-cont">
                 <div>
-                    {/* {messages} */}
                     <p>
-                        All Messages
+                        {this.state.messages.length < 1 && (<span>You have no message history.</span>)}
+
+                        {this.state.messages && allMsgs}
                     </p>
                 </div>
             </div>
